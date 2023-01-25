4c49fe9674d962a1ab45165fe80410c58fd81c7f
ronapats <ronapat.srisupavanich@kellogg.northwestern.edu>
2023-01-25 21:06:44 +0000

    practice3

--- labs/4-hashes.rb
+++ labs/4-hashes.rb
@@ -16,7 +16,6 @@ bitcoin_data = JSON.parse(response)
 # Ask the user for the current amount of Bitcoin that they own.
 # Using the Ruby hash `bitcoin_data`, display a summary of
 # Bitcoin data for the user. Something like the output below.
-
 # Sample output:
 # 1 Bitcoin is valued at $41405.1046 USD.
 # Your Bitcoin is worth $62107.6569.
@@ -28,16 +27,27 @@ bitcoin = gets.chomp
 # 2. The value will be a string, so you'll want to convert it to a Float.
 bitcoin = bitcoin.to_f
 
-# 3. inspect the bitcoin_data hash
-puts bitcoin_data.keys
+# Ask currency
+puts "Which currrency do you want? (USD,GBP,EUR)"
+    currency = gets.chomp
 
-# 4. store the rate in USD of bitcoin
-rate = bitcoin_data["bpi"]["USD"]["rate_float"]
-
-# 5. calculate the value of the user's bitcoin in USD
-value = rate * bitcoin
-puts value
-
-# 6. output the rate and value:
-puts "1 Bitcoin is valued at $#{rate}."
-puts "Your Bitcoin is worth $#{value}."
+# 3. store the rate in USD/GBP/EUR of bitcoin
+if currency == "USD"
+    bitcoin_rate = bitcoin_data["bpi"]["USD"]["rate_float"]
+    value = bitcoin_rate * bitcoin
+    puts "1 Bitcoin is valued at USD#{bitcoin_rate}."
+    puts "Your Bitcoin is worth USD#{value}."
+elsif currency == "GBP"
+    bitcoin_rate = bitcoin_data["bpi"]["GBP"]["rate_float"]
+    value = bitcoin_rate * bitcoin
+    puts "1 Bitcoin is valued at GBP#{bitcoin_rate}."
+    puts "Your Bitcoin is worth GBP#{value}."
+elsif currency == "EUR"
+    bitcoin_rate = bitcoin_data["bpi"]["EUR"]["rate_float"]
+    value = bitcoin_rate * bitcoin
+    puts "1 Bitcoin is valued at EUR#{bitcoin_rate}."
+    puts "Your Bitcoin is worth EUR#{value}."
+else 
+    puts "N/A, type currency again in USD/GBP/EUR"
+    currency = gets.chomp
+end
